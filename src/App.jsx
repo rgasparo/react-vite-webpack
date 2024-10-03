a29fc75a002e8949ff4adbd0635882118fa530c9
rgasparo <119346083+rgasparo@users.noreply.github.com>
2024-10-03 14:35:33 -0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <h1>App React + VITE + WEBPACK</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/main.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve('public'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  mode: 'development',
+};
