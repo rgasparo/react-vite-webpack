4e6d8efea512fad49cfd3160d97de5e1475dc1b6
rgasparo <119346083+rgasparo@users.noreply.github.com>
2024-10-04 11:16:09 -0600

    consumiendo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { Suspense, lazy } from 'react';
+
+const PaymentForm = lazy(() => import('pagos/PaymentForm'));
+
 const App = () => {
   return (
+    <>
     <div>
       <h1>App React + VITE + WEBPACK</h1>
       <h2>This is vite deploy</h2>
     </div>
+      <Suspense fallback={<p>Loading Payment Form...</p>}>
+        <PaymentForm title={'Formulario desde remoto (react+vite)'}/>
+      </Suspense>
+    </>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'mf-react-vite', // Nombre de la aplicación
+      filename: 'remoteEntry.js', // Nombre del archivo remoto
+      exposes: {
+        // Componentes o módulos que serán consumidos por otras apps
+        './ComponentName': './src/components/ComponentName.jsx',
+      },
+      remotes: {
+        // Consumir otros microfrontends:
+        'pagos': 'https://pasarela-de-pagos.vercel.app/assets/remoteEntry.js'
+      },
+      shared: ['react', 'react-dom'], // Dependencias compartidas
+    }),
+  ],
+  build: {
+    target: 'esnext',
+  },
+});
